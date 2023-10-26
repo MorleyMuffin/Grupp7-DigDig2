488894d577abaeb91e515dcb00ffbb0e83808922
Noah-Hindborg <noah.hindborg@elev.ga.lbs.se>
2023-10-26 13:25:04 +0200

    Improved shooting

--- Assets/ThirdPersonShooterController.cs
+++ Assets/ThirdPersonShooterController.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
@@ -19,7 +23,23 @@ public class ThirdPersonShooterController : MonoBehaviour
     [SerializeField] private float weaponDamage = 3f;
 
     [SerializeField] float shootingWaitTime = 1f;
-    [SerializeField] bool canShoot;
+    [SerializeField] float currentShootingWaitTime;
+    [SerializeField] bool canShoot = true;
+
+    [SerializeField] bool isShooting = false;
+    [SerializeField] float shootingTimer = 0.0f;
+
+    [SerializeField] bool canFire = true; // erm isnt this just the same thing as canShoot?
+
+    [SerializeField] int maxBullets = 10;
+    [SerializeField] int currentBulletAmount;
+    [SerializeField] TextMeshProUGUI bulletDisplay; // Change to show x amount of bullets instead of text
+
+
+
+    [SerializeField] Slider canShootBar;
+    //Ádd bullet amount
+
 
     [SerializeField] private Transform vfxHitGreen;
     [SerializeField] private Transform vfxHitRed;
@@ -28,15 +48,19 @@ public class ThirdPersonShooterController : MonoBehaviour
     private StarterAssetsInputs starterAssetInputs;
 
     private EnemyHealth enemyHealth;
-    
 
     private void Awake()
     {
+        currentBulletAmount = maxBullets;
+
+        canShootBar.maxValue = shootingWaitTime; 
+
         thirdPersonController = GetComponent<ThirdPersonController>();
         starterAssetInputs = GetComponent<StarterAssetsInputs>();
     }
     void Update()
     {
+        #region Aiming
         Vector3 mouseWorldPosition = Vector3.zero;
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
@@ -68,34 +92,86 @@ public class ThirdPersonShooterController : MonoBehaviour
             thirdPersonController.SetCameraSensitivity (normalCameraSensitivity);
             thirdPersonController.SetRotationOnMove(true);
         }
+        #endregion 
 
-        if  (starterAssetInputs.shoot)
+        #region Shooting
+
+        if (currentBulletAmount <= 0)
         {
-            if(hitTransform != null) 
-            {
-                if(hitTransform.GetComponent<EnemyHealth>() != null)
-                {
-                    Instantiate(vfxHitRed, raycastHit.point, Quaternion.identity); enemyHealth = hitTransform.GetComponent<EnemyHealth>(); enemyHealth.DamageToEnemy(weaponDamage);
-                }
-                else
-                {
-                    Instantiate(vfxHitGreen, raycastHit.point, Quaternion.identity);
-                }
-            }
-            starterAssetInputs.shoot = false;
+            canShoot = false;
         }
+        if (Input.GetKeyDown(KeyCode.R))// temperary
+        {
+            currentBulletAmount = maxBullets; // Add loading time to do this
+            canShoot = true;
+        }
+
+        if  (starterAssetInputs.shoot && canFire)
+        {
+            if (canShoot == true)
+            {
+                isShooting = true;
+                shootingTimer = 0.0f;
+
+                if (hitTransform != null)
+                {
+                    if (hitTransform.GetComponent<EnemyHealth>() != null)
+                    {
+                        Instantiate(vfxHitRed, raycastHit.point, Quaternion.identity); 
+                        enemyHealth = hitTransform.GetComponent<EnemyHealth>(); 
+                        enemyHealth.DamageToEnemy(weaponDamage);
+                    }
+                    else
+                    {
+                        Instantiate(vfxHitGreen, raycastHit.point, Quaternion.identity);
+                    }
+                }
+
+                currentBulletAmount--;
+
+                starterAssetInputs.shoot = false;
+                StartCoroutine(TimeUntilCanShoot());
+
+               // canShoot = false;
+               canFire = false;
+            }
+        }
+        bulletDisplay.text = ($"bullets {currentBulletAmount}");
+
+        if (isShooting)
+        {
+            // Increment shooting timer
+            shootingTimer += Time.deltaTime;
+
+            // Calculate the interpolation factor (0 to 1)
+            float t = Mathf.Clamp01(shootingTimer / shootingWaitTime);
+
+            // Interpolate the value using SmoothStep (you can also use Lerp)
+            currentShootingWaitTime = Mathf.SmoothStep(0.0f, shootingWaitTime, t);
+
+            // Check if shooting time has reached the max
+            if (shootingTimer >= shootingWaitTime)
+            {
+                isShooting = false;  // Reset shooting flag
+
+                //canShoot = true;
+                canFire = true;
+            }
+        }
+
+
+        canShootBar.value = currentShootingWaitTime;
+        #endregion
 
     }
 
- /*private IEnumerator TimeUntilCanShoot()  
+    private IEnumerator TimeUntilCanShoot()  
     {
-        if(canShoot == false)
-        {
-          //return;
-        }
-        else
-        {
-
-        }
-    }*/
+        canShoot = false;
+        Debug.Log("Cant shoott");
+        yield return new WaitForSeconds(shootingWaitTime);
+        canShoot = true;
+        Debug.Log("Can shoooot");
+    }
+    
 }
